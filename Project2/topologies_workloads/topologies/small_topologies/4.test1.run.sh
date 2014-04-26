57b263d3e39e40b55cdf4b6eca57deae981e8d65
Steve Sun <steve.j.sun@gmail.com>
2014-04-26 13:27:25 -0500

    moved Project2 to a new repo

--- Project2/topologies_workloads/topologies/small_topologies/4.test1.run.sh
+++
@@ -1,6 +0,0 @@
-../../../veil_switch.pyc 4.adlist 4.vid localhost:5001 &
-../../../veil_switch.pyc 4.adlist 4.vid localhost:5002 &
-../../../veil_switch.pyc 4.adlist 4.vid localhost:5003 &
-../../../veil_switch.pyc 4.adlist 4.vid localhost:5004 &
-../../../traffic-gen.pyc 4.vid 4.test1.workload localhost:5001 &
-../../../traffic-gen.pyc 4.vid 4.test1.workload localhost:5004
--- Project2/topologies_workloads/topologies/small_topologies/8.no.workload.run.sh
+++
@@ -1,8 +0,0 @@
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6001 &
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6002 &
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6003 &
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6004 &
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6005 &
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6006 &
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6007 &
-../../../veil_switch.pyc 8.adlist 8.vid localhost:6008 
